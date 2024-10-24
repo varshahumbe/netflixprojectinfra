a32bad029d74782c08e86f993e7845a85d777494
varshahumbe <infovarshahumbe@gmail.com>
2024-10-24 07:23:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "one" {
 count = 4
 ami = "ami-0cf4e1fcfd8494d5b"
 instance_type = "t2.medium"
-key_name = "rahull"
+key_name = "varsha"
 vpc_security_group_ids = ["sg-042aea60981b72321"]
 tags = {
 Name = var.instance_names[count.index]

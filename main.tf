5cef56f97064504c38843f7ad872b9cbfb51fa71
varshahumbe <infovarshahumbe@gmail.com>
2024-10-25 13:13:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ region = "us-west-1"
 
 resource "aws_instance" "one" {
 count = 4
-ami = "ami-0cf4e1fcfd8494d5b"
+ami = "ami-0175bdd48fdb0973b"
 instance_type = "t2.medium"
 key_name = "varsha"
 vpc_security_group_ids = ["sg-042aea60981b72321"]
